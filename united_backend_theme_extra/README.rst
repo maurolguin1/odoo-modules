4e2aceacaafe266a6ba751c43ce6bbe878c5a3f8
Mehdi Ghezal <mehdi.ghezal@gmail.com>
2016-11-07 14:43:11 +0800

    Add new module united_backend_theme_extra

---
+++ united_backend_theme_extra/README.rst
@@ -0,0 +1,24 @@
+.. image:: https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000
+   :target: https://opensource.org/licenses/mit-license.php
+   :alt: License: MIT
+
+==========================
+United Backend Theme Extra
+==========================
+
+This module add extra styles to United Backend Theme.
+
+Installation
+============
+
+No external library required.
+
+Configuration
+=============
+
+No configuration required.
+
+Credits
+=======
+
+Critech Limited <contact@critech-services.com>
---
+++ united_backend_theme_extra/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Critech Limited (contact@critech-services.com)
+# License MIT (https://opensource.org/licenses/mit-license.php)
+
+{
+    'name': 'United Backend Theme Extra',
+    'summary': 'This module add extra styles to United Backend Theme',
+    'author': 'Critech Limited',
+    'website': 'http://www.critech-services.com',
+    'category': 'Specific Module',
+    'version': '9.0.1.0.0',
+    'license': 'Other OSI approved licence',
+    'depends': [
+        'base',
+        'united_backend_theme',
+    ],
+    'data': [
+        'views/styles.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
